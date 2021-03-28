483ff35d44385f758c753b86f3f81554b194bff9
BrandoSoft <csbrandox@gmail.com>
2021-03-28 22:07:56 +0200

    deployment on netlify not working

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const catPaw = L.icon({
   shadowSize: [50, 64], // size of the shadow
   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
   shadowAnchor: [4, 62],  // the same for the shadow
-  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  popupAnchor: [-3, -75] // point from which the popup should open relative to the iconAnchor
 });
 
 const Markers = () => {
