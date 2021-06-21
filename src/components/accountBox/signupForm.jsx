bcbecfb077839deef21bd91ad4891749626218d8
BrandoSoft <csbrandox@gmail.com>
2021-06-21 21:39:51 +0200

    adding axios register form is working with backen

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -9,20 +9,82 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import axios from "axios";
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
 
+  const apiUrl = "http://localhost:3000/user/register";
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    pwd: "",
+  });
+
+  function loginHandler(e) {
+    e.preventDefault();
+
+    axios
+      .post(apiUrl, {
+        name: data.name,
+        email: data.email,
+        pwd: data.pwd,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+
+    // axios
+    //   .post("localhost:3000/user/register", {
+    //     name: data.name,
+    //     email: data.email,
+    //     pwd: data.pwd,
+    //   })
+
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+
+    // axios.get("http://localhost:3000/user/message");
+  }
+
+  function onChangeHandler(e) {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  }
+
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input
+          onChange={(e) => onChangeHandler(e)}
+          id="name"
+          value={data.name}
+          type="text"
+          placeholder="Name"
+        />
+        <Input
+          onChange={(e) => onChangeHandler(e)}
+          id="email"
+          value={data.email}
+          placeholder="Email"
+          type="email"
+        />
+        <Input
+          onChange={(e) => onChangeHandler(e)}
+          id="pwd"
+          value={data.pwd}
+          type="text"
+          placeholder="Password"
+        />
         <Input type="password" placeholder="Confirm Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={(e) => loginHandler(e)}>
+        Signup
+      </SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
